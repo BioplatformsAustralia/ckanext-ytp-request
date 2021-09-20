b79c02a0e4fdee336a2a10cbe6aa211b1da1526b
Mark Tearle <mark.tearle@qcif.edu.au>
2021-09-20 12:18:56 +0800

    Remove unused javascript

--- ckanext/ytp_request/public/javascript/request.js
+++ ckanext/ytp_request/public/javascript/request.js
@@ -8,22 +8,6 @@ $(document).ready(function() {
       approve_url_element.attr('href', newapp_url);
     });
     
-    $('#field-organizations').on('change', function(e) {
-        //Setting the default selected roleto admin (in case editor was selected and now does not exist)
-    var roles = $('#role');
-        roles.val("admin").trigger("change");
-        ckan.sandbox().client.call('POST', 'get_available_roles', {organization_id: $("#field-organizations option:selected").text()}, function(results) {
-          var roles = $('#role');
-          //Remove old values
-          roles.get(0).options.length = 0;
-          //Populate with news
-          var result = results['result'];
-          $.each(result, function(key, val) {
-            roles.get(0).options[roles.get(0).options.length] = new Option(val.text, val.value);
-          }); 
-        });
-   }); 
-
    $('#memberRequestModal').on('show.bs.modal', function (event) {
      var button = $(event.relatedTarget) // Button that triggered the modal
      var orgname = button.data('orgname') // Extract info from data-* attributes
