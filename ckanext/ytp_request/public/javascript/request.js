04cd48c1a0675ae547bd098eeed9241b253b4407
Mark Tearle <mark.tearle@qcif.edu.au>
2021-09-22 10:54:53 +0800

    Use organization display name on Request Reject modal

--- ckanext/ytp_request/public/javascript/request.js
+++ ckanext/ytp_request/public/javascript/request.js
@@ -29,10 +29,11 @@ $(document).ready(function() {
      var action = button.data('action') // Extract info from data-* attributes
      var username = button.data('username') // Extract info from data-* attributes
      var organization = button.data('organization') // Extract info from data-* attributes
+     var orgname = button.data('orgname') // Extract info from data-* attributes
      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
      var modal = $(this)
-     modal.find('.modal-details').text(organization + ' from ' + username)
+     modal.find('.modal-details').text(orgname + ' from ' + username)
      $('#membership-request-reject-form').attr('action', action)
    });
 });
